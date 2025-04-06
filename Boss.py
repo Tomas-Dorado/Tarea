b24e49145733f0d79143e8291ab912666b40ac95
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-06 19:59:51 +0000

    Se creo la clase boss

---
+++ Boss.py
@@ -0,0 +1,13 @@
+from Opponent import Opponent
+
+class Boss(Opponent):
+    def __init__(self, x, y, image=None):
+        super().__init__(x, y, image)
+        self.lives = 5  
+        self.speed = 4  
+
+    def move(self, SCREEN_WIDTH=800, SCREEN_HEIGHT=600):
+        self.y += self.speed
+        if self.y > SCREEN_HEIGHT:
+            self.y = -50
+            self.x = random.randint(0, SCREEN_WIDTH - 50)
